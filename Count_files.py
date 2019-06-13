7a70ed4eb2bbe2233d6eaa483048b7a2435d2fcc
Vaitesh <40633977+Vaitesh@users.noreply.github.com>
2019-06-13 19:41:23 +0530

    Update Count_files.py

--- Count_files.py
+++ Count_files.py
@@ -5,3 +5,9 @@ print(len[filename for filename in os.listdir('.') if os.path.isfile(filename)])
 #to determine the files with naming of other versions
 dir = '/tmp'
 print(len[filename for filename in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+
+#to recursively find the files in the subdirectory use os.walk
+for (root,dirs,files) in os.walk(dir, topdown=true):
+  print(root)
+  print(dirs)
+  print(files)
