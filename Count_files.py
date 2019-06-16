980189d7c3fa7dd802864d78c22809d1d228e1d0
Vaitesh <40633977+Vaitesh@users.noreply.github.com>
2019-06-16 08:43:56 +0530

    Update Count_files.py

--- Count_files.py
+++ Count_files.py
@@ -11,3 +11,32 @@ for (root,dirs,files) in os.walk(dir, topdown=true):
   print(root)
   print(dirs)
   print(files)
+
+  #working code to get the list of files present in zip archive
+  import os, os.path
+
+directory = input('Please specify the path: ')
+print(directory)
+#print(len([filename for filename in os.listdir(directory) if os.path.isfile(directory)]))
+print(len([filename for filename in os.listdir(directory)])) #if os.path.isfile(filename)]))
+
+#to determine the files with naming of other versions
+#dir = '/tmp'
+#print(len[filename for filename in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+
+from zipfile import ZipFile, is_zipfile, ZipInfo
+
+#file_to_find = input('Please specify the filename')
+for filename in os.listdir(directory):
+    if is_zipfile(filename):
+        with ZipFile(filename, 'r') as zip:
+            #if File name = file_to_find:
+                #print('Found')
+            zip.printdir()
+            #class ZipInfo(filename,(1980, 1, 1, 0, 0, 0)):
+                #def __init__(self):
+                    #list_of_files = infolist()
+
+            #list_of_files = ZipInfo()
+
+#creating class for zipinfo
